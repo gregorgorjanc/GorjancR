9591fb35fe56b4d26b79a04a3f4051c06b91ef4e
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2016-11-21 16:53:26 +0000

    SelInt

---
+++ R/SelInt.R
@@ -0,0 +1,22 @@
+
+#' Selection intensity.
+#'
+#' \code{SelInt} evaluates selection intensity given proportion of selected individuals.
+#'
+#' @param x numeric, proportion of selected individuals.
+#'
+#' @return Selection intensity.
+#'
+#' @examples
+#' SelInt(x=1.00)
+#' SelInt(x=0.99)
+#' SelInt(x=0.50)
+#' SelInt(x=0.05)
+#' SelInt(x=0.01)
+#' @export
+SelInt <- function(x) {
+  if(any(x < 0) | any(x > 1)) stop("'x' must be in range [0, 1]")
+  dnorm(qnorm(x, lower.tail=FALSE)) / x
+  ## qnorm() maps [0, 1] to N(0, 1)
+  ## dnorm() specifys the height i.e. z
+}
